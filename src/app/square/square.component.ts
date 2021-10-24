7c2b97a16b033a5351c8cbf8dc24db6758939912
Qphuc <tqp1610@gmail.com>
2021-10-24 18:48:23 +0700

    square

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,6 +1,13 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { KonvaComponent } from 'ng2-konva';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  fromEvent,
+  Observable,
+  of,
+} from 'rxjs';
+import { pairwise, map, startWith, scan, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-square',
@@ -83,4 +90,78 @@ export class SquareComponent implements OnInit {
     this.lastPosY = circle.y;
   }
 
+  // private scale: Observable<any> = fromEvent<WheelEvent>(window, 'wheel').pipe(
+  //   map((e) => e.deltaY),
+  //   scan((oldScale, deltaY) => {
+  //     console.log('scale', oldScale);
+  //     const scaleBy = 1.5;
+  //     return deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+  //   }, 1)
+  // );
+
+  // private position = this.scale.pipe(
+  //   pairwise(),
+  //   scan(
+  //     (oldPosition: any, [oldScale, newScale]) => {
+  //       console.log('test', oldPosition);
+  //       const stage = this.stage.getStage();
+
+  //       const absoluteMouse = stage.getPointerPosition();
+  //       if (!absoluteMouse) {
+  //         return oldPosition;
+  //       }
+  //       const relativeMouse = {
+  //         x: absoluteMouse.x / oldScale - stage.x() / oldScale,
+  //         y: absoluteMouse.y / oldScale - stage.y() / oldScale,
+  //       };
+  //       return {
+  //         x: -(relativeMouse.x - absoluteMouse.x / newScale) * newScale,
+  //         y: -(relativeMouse.y - absoluteMouse.y / newScale) * newScale,
+  //       };
+  //     },
+  //     { x: 0, y: 0 }
+  //   )
+  // );
+
+  // private size = fromEvent(window, 'resize').pipe(
+  //   map(() => {
+  //     const container = this.container.nativeElement;
+  //     return {
+  //       height: container.offsetHeight,
+  //       width: container.offsetWidth,
+  //     };
+  //   })
+  // );
+
+  // public configStage = combineLatest(
+  //   this.scale.pipe(startWith(1)),
+  //   this.position.pipe(startWith({ x: 0, y: 0 })),
+  //   this.size
+  // ).pipe(
+  //   map(([scale, position, size]) => ({
+  //     draggable: true,
+  //     scaleX: scale,
+  //     scaleY: scale,
+  //     ...position,
+  //     ...size,
+  //   })),
+  //   startWith({
+  //     height: 1000,
+  //     width: 1000,
+  //   })
+  // );
+
+  // ngAfterViewInit() {
+  // Resize Canvas as soon as we can get container width and height
+  // Also IE Comp
+  // const event = document.createEvent('UIEvents');
+  // event.initEvent('resize', false, false);
+  // window.dispatchEvent(event);
+
+  // const test = new Hammer(this.container.nativeElement, {
+  //   recognizers: [[Pinch]],
+  // });
+
+  // test.on('pinch', (e:any) => alert(JSON.stringify(e)));
+  // }
 }
